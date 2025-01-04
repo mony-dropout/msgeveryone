11e9c3cfbdd9a3a0a9e1f06b5718a34cecc85492
mony <graphtheorykid@gmail.com>
2025-01-05 02:45:28 +0530

    hows this bruh

--- learnsel.py
+++ learnsel.py
@@ -1,11 +1,10 @@
-#just learn selenium right now
+#we can do anything bruh, for real, dropoutin2 ;)
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
 driver=webdriver.Chrome()
 driver.get("https://google.com")
-time.sleep(5)
-
+time.sleep(6)
 driver.quit()
+
