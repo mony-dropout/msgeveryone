caa8b325f74924d461cfb715b262c305e36a3647
mony <graphtheorykid@gmail.com>
2025-01-07 04:00:55 +0530

    html class/div

--- learnsel.py
+++ learnsel.py
@@ -1,10 +1,8 @@
-#we can do anything bruh, for real, dropoutin2 ;)
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 driver=webdriver.Chrome()
-driver.get("https://google.com")
+driver.get("file:///Users/monyatwu/Documents/first%20project/learnhtml.html")
 time.sleep(6)
 driver.quit()
-
