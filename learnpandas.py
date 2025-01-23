b90ac7d97db5c13411e4daeb9482fe63b3ce8732
mony <graphtheorykid@gmail.com>
2025-01-24 03:22:49 +0530

    working final stretch

--- learnpandas.py
+++ learnpandas.py
@@ -1,3 +1,8 @@
 import pandas as pd
-nameage=pd.read_csv("learncsv.csv")
-print(nameage.iloc[1])
+df=pd.read_csv("full_contacts.csv")
+fdf=pd.DataFrame()
+fdf['name']=df['First Name']
+fdf['phone']=df['Home Phone']
+fdf.to_csv('bestever.csv',index=False)
+
+
